cd40f2503d41b5734bf82808f6ad8199de6bae84
akhtarfahad7 <akhtarfahad7@users.noreply.github.com>
2026-09-13 13:20:54 +0500

    feat: 2-tier pricing (Free + Pro) aligned app & website

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1687668920" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2167656903" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
